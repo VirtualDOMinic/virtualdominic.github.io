e8e9aa2d1c1170b91b3f04c40621ae8b8769a29f
Dominic C <domjc@live.com>
2017-11-28 13:55:07 +0000

    new tab/nav trial

--- js/nav.js
+++ js/nav.js
@@ -10,3 +10,27 @@
         }
 
             window.addEventListener("scroll",fixNav); //run fixNav function whenever I scroll
+
+
+//tabs start
+function openSection(evt, secName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(secName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
