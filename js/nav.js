c905971ed4009e91b4dbabb981167665fbe51e86
Dominic C <domjc@live.com>
2017-11-28 18:53:35 +0000

    more formatting & content
    not quite finished, but mostly there with the exception of content. Added images and a horizontal container. Updated font sizes. May want to normalise paragraph font size (currently 1.7rem)

--- js/nav.js
+++ js/nav.js
@@ -1,36 +1,39 @@
  //Sticky nav start
- const nav = document.querySelector("#topNav"); //get nav (id is "topNav" - Wes goes with "main" in his tute)
-    const topOfNav = nav.offsetTop //detect where top of the nav is
-    function fixNav(){
-         if(window.scrollY >= topOfNav){
-            document.body.classList.add("fixed-nav");
-         } else {
-            document.body.classList.remove("fixed-nav")
-            }
+const nav = document.querySelector("#topNav"); //get nav (id is "topNav" - Wes goes with "main" in his tute)
+const topOfNav = nav.offsetTop //detect where top of the nav is
+function fixNav(){
+    if(window.scrollY >= topOfNav){
+        document.body.style.paddingTop = nav.offsetHeight + "px";
+        document.body.classList.add("fixed-nav");
+    } else {
+        document.body.style.paddingTop = 0;
+        document.body.classList.remove("fixed-nav")
         }
+    }
 
-            window.addEventListener("scroll",fixNav); //run fixNav function whenever I scroll
+window.addEventListener("scroll",fixNav); //run fixNav function whenever I scroll
 
-
+//sticky nav end
 //tabs start
 function openSection(evt, secName) {
-    // Declare all variables
+    // Declare/initialise vars
     var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
+    // Hide all elements with class="tabcontent"
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    // Get all elements with class="tablinks" and remove the class "active"
+    // Remove "active" class from all elements with class="tablinks"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
+    // Show current tab, and add "active" class to the relevant button
     document.getElementById(secName).style.display = "block";
     evt.currentTarget.className += " active";
 }
 
+//tabs end
