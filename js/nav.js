5ed155768f2ba22df801ce2ef5ea8ead8d88cea3
Dominic C <domjc@live.com>
2017-11-29 21:14:25 +0000

    li instead of p?

--- js/nav.js
+++ js/nav.js
@@ -1,23 +1,4 @@
-//Sticky nav start
-/*
-const nav = document.querySelector("#topNav"); //get nav (id is "topNav" - Wes goes with "main" in his tute)
-const topOfNav = nav.offsetTop //detect where top of the nav is
-function fixNav(){
-    if(window.scrollY >= topOfNav){
-        document.body.style.paddingTop = nav.offsetHeight + "px";
-        document.body.classList.add("fixed-nav");
-    } else {
-        document.body.style.paddingTop = 0;
-        document.body.classList.remove("fixed-nav")
-        }
-    }
-
-window.addEventListener("scroll",fixNav); //run fixNav function whenever I scroll
-*/
-//sticky nav end - can delete the above? No use for it now
-
-
-//tabs start
+//nav tabs start - time for Dom to manipulate the DOM
 function openSection(evt, secName) {
     // Declare/initialise vars
     var i, tabcontent, tablinks;
@@ -38,5 +19,4 @@ function openSection(evt, secName) {
     document.getElementById(secName).style.display = "block";
     evt.currentTarget.className += " active";
 }
-
 //tabs end
