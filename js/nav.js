caff52ea5ba26bb94b1cf8ef09020e4ea022a742
Dominic C <domjc@live.com>
2018-03-08 23:49:52 +0000

    prettier

--- js/nav.js
+++ js/nav.js
@@ -1,22 +1,22 @@
 //nav tabs start - time for Dom to manipulate the DOM
 function openSection(evt, secName) {
-    // Declare/initialise vars
-    var i, tabcontent, tablinks;
+  // Declare/initialise vars
+  var i, tabcontent, tablinks;
 
-    // Hide all elements with class="tabcontent"
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+  // Hide all elements with class="tabcontent"
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
 
-    // Remove "active" class from all elements with class="tablinks"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+  // Remove "active" class from all elements with class="tablinks"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
 
-    // Show current tab, and add "active" class to the relevant button
-    document.getElementById(secName).style.display = "block";
-    evt.currentTarget.className += " active";
+  // Show current tab, and add "active" class to the relevant button
+  document.getElementById(secName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 //tabs end
